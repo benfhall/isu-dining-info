a915fbab0a28758a9e173a4e06f5d1639a892b75
Ben Hall <swidexx@gmail.com>
2021-02-13 17:12:30 -0600

    reduced complexity, 3hr auto-reload, reload command

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os, discord, urllib.request, urllib.error, urllib.parse, json, ssl
 from dotenv import load_dotenv
+from discord.ext import tasks
 
 url_udcc="https://www.dining.iastate.edu/wp-json/dining/menu-hours/get-single-location/?slug=union-drive-marketplace-2-2"
 url_windows="https://www.dining.iastate.edu/wp-json/dining/menu-hours/get-single-location/?slug=friley-windows-2-2"
@@ -14,75 +15,11 @@ headers = {
         'Connection': 'keep-alive'
     }
 
-udcc_cc_b, udcc_cc_l, udcc_cc_d, udcc_p_b, udcc_p_l, udcc_p_d, udcc_d_b, udcc_d_l, udcc_d_d, udcc_pa_b, udcc_pa_l, udcc_pa_d = [],[],[],[],[],[],[],[],[],[],[],[]
-windows_s_l, windows_s_d, windows_z_l, windows_z_d, windows_sl_l, windows_sl_d, windows_sv_l, windows_sv_d = [],[],[],[],[],[],[],[]
-gcontext = ssl.SSLContext()
-request=urllib.request.Request(url_windows,None,headers)
-with urllib.request.urlopen(request, context=gcontext) as url:
-    data = json.loads(url.read().decode())
-
-for time in data[0]["menus"]:
-    for bar in time["menuDisplays"]:
-        for food in bar['categories'][0]['menuItems']:
-            if bar['name'] == "Simmer":
-                if time['section'] == "Lunch":
-                    windows_s_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    windows_s_d.append(food['name'])
-            if bar['name'] == "Zest":
-                if time['section'] == "Lunch":
-                    windows_z_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    windows_z_d.append(food['name'])
-            if bar['name'] == "Slice":
-                if time['section'] == "Lunch":
-                    windows_sl_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    windows_sl_d.append(food['name'])
-            if bar['name'] == "Savor":
-                if time['section'] == "Lunch":
-                    windows_sv_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    windows_sv_d.append(food['name'])
-
-request=urllib.request.Request(url_udcc,None,headers)
-with urllib.request.urlopen(request) as url:
-    data = json.loads(url.read().decode())
-
-for time in data[0]["menus"]:
-    for bar in time["menuDisplays"]:
-        for food in bar['categories'][0]['menuItems']:
-            if bar['name'] == "Cardinal Canteen":
-                if time['section'] == "Breakfast":
-                    udcc_cc_b.append(food['name'])
-                if time['section'] == "Lunch":
-                    udcc_cc_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    udcc_cc_d.append(food['name'])
-            if bar['name'] == "Picoso":
-                if time['section'] == "Breakfast":
-                    udcc_p_b.append(food['name'])
-                if time['section'] == "Lunch":
-                    udcc_p_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    udcc_p_d.append(food['name'])
-            if bar['name'] == "Dagwood's":
-                if time['section'] == "Breakfast":
-                    udcc_d_b.append(food['name'])
-                if time['section'] == "Lunch":
-                    udcc_d_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    udcc_d_d.append(food['name'])
-            if bar['name'] == "Parma's":
-                if time['section'] == "Breakfast":
-                    udcc_pa_b.append(food['name'])
-                if time['section'] == "Lunch":
-                    udcc_pa_l.append(food['name'])
-                if time['section'] == "Dinner":
-                    udcc_pa_d.append(food['name'])
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+udcc_cc, udcc_p, udcc_d, udcc_pa = [[] for r in range(3)],[[] for r in range(3)],[[] for r in range(3)],[[] for r in range(3)]
+windows_s, windows_z, windows_sl, windows_sv = [[] for r in range(2)],[[] for r in range(2)],[[] for r in range(2)],[[] for r in range(2)]
 
 client = discord.Client()
 
@@ -90,116 +27,176 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@tasks.loop(seconds=10800)
+async def load_menus():
+    global udcc_cc
+    global udcc_p
+    global udcc_d 
+    global udcc_pa
+    global windows_s
+    global windows_z
+    global windows_sl
+    global windows_sv
+
+    print('Reloading Menus...')
+    gcontext = ssl.SSLContext()
+    request=urllib.request.Request(url_windows,None,headers)
+    with urllib.request.urlopen(request, context=gcontext) as url:
+        data = json.loads(url.read().decode())
+
+    udcc_cc, udcc_p, udcc_d, udcc_pa = [[] for r in range(3)],[[] for r in range(3)],[[] for r in range(3)],[[] for r in range(3)]
+    windows_s, windows_z, windows_sl, windows_sv = [[] for r in range(2)],[[] for r in range(2)],[[] for r in range(2)],[[] for r in range(2)]
+    options_windows = {'Lunch': 0, 'Dinner': 1}
+    for time in data[0]["menus"]:
+        for bar in time["menuDisplays"]:
+            for food in bar['categories'][0]['menuItems']:
+                if bar['name'] == "Simmer":
+                    windows_s[options_windows.get(time['section'])].append(food['name'])
+                if bar['name'] == "Zest":
+                    windows_z[options_windows.get(time['section'])].append(food['name'])
+                if bar['name'] == "Slice":
+                    windows_sl[options_windows.get(time['section'])].append(food['name'])
+                if bar['name'] == "Savor":
+                    windows_sv[options_windows.get(time['section'])].append(food['name'])
+
+    request=urllib.request.Request(url_udcc,None,headers)
+    with urllib.request.urlopen(request) as url:
+        data = json.loads(url.read().decode())
+
+    options_udcc = {'Breakfast': 0, 'Lunch': 1, 'Dinner': 2}
+    for time in data[0]["menus"]:
+        for bar in time["menuDisplays"]:
+            for food in bar['categories'][0]['menuItems']:
+                if bar['name'] == "Cardinal Canteen":
+                    udcc_cc[options_udcc.get(time['section'])].append(food['name'])
+                if bar['name'] == "Picoso":
+                    udcc_p[options_udcc.get(time['section'])].append(food['name'])
+                if bar['name'] == "Dagwood's":
+                    udcc_d[options_udcc.get(time['section'])].append(food['name'])
+                if bar['name'] == "Parma's":
+                    udcc_pa[options_udcc.get(time['section'])].append(food['name'])
+    print("Complete!")
+
 @client.event
 async def on_message(message):
+    PICOSO = "\n**Picoso**:\n"
+    DAGWOOD = "\n**Dagwood's**:\n"
+    PARMA = "\n**Parma's**:\n"
+    SLICE = "\n**Slice**:\n"
+    ZEST = "\n**Zest**:\n"
+    SAVOR = "\n**Savor**:\n"
+    if '!reload' in message.content.lower():
+        load_menus()
+        await message.channel.send("Reloaded Menus!")
     if '!help' in message.content.lower():
-        response = "!**<time>**_**<building>** - *find the menu for given <time> at given <building>*\n\n**<time>** - Breakfast/Lunch/Dinner *(breakfast not available for Windows)*\n\n**<building>** - windows/udcc"
+        response = "!**<time>**_**<building>** - *find the menu for given <time> at given <building>*\n\n**<time>** - Breakfast/Lunch/Dinner *(breakfast not available for Windows)*\n\n**<building>** - windows/udcc\n\n!**reload** - *reloads the menu.*"
         await message.channel.send(response)
     if '!breakfast_udcc' in message.content.lower():
         response = "**Breakfast** at **Union Market Drive:** \n\n**Cardinal Canteen**: \n"
-        for food in udcc_cc_b:
+        for food in udcc_cc[0]:
             response += food
             response += "\n"
 
-        response += "\n**Picoso**:\n"
-        for food in udcc_p_b:
+        response += PICOSO
+        for food in udcc_p[0]:
             response += food
             response += "\n"
 
-        response += "\n**Dagwood's**:\n"
-        for food in udcc_d_b:
+        response += DAGWOOD
+        for food in udcc_d[0]:
             response += food
             response += "\n"
 
-        response += "\n**Parma's**:\n"
-        for food in udcc_pa_b:
+        response += PARMA
+        for food in udcc_pa[0]:
             response += food
             response += "\n"
         
         await message.channel.send(response)
     if '!lunch_udcc' in message.content.lower():
         response = "**Lunch** at **Union Market Drive:** \n\n**Cardinal Canteen**: \n"
-        for food in udcc_cc_l:
+        for food in udcc_cc[1]:
             response += food
             response += "\n"
 
-        response += "\n**Picoso**:\n"
-        for food in udcc_p_l:
+        response += PICOSO
+        for food in udcc_p[1]:
             response += food
             response += "\n"
 
-        response += "\n**Dagwood's**:\n"
-        for food in udcc_d_l:
+        response += DAGWOOD
+        for food in udcc_d[1]:
             response += food
             response += "\n"
 
-        response += "\n**Parma's**:\n"
-        for food in udcc_pa_l:
+        response += PARMA
+        for food in udcc_pa[1]:
             response += food
             response += "\n"
         await message.channel.send(response)
     if '!dinner_udcc' in message.content.lower():
         response = "**Dinner** at **Union Market Drive:** \n\n**Cardinal Canteen**: \n"
-        for food in udcc_cc_d:
+        for food in udcc_cc[2]:
             response += food
             response += "\n"
 
-        response += "\n**Picoso**:\n"
-        for food in udcc_p_d:
+        response += PICOSO
+        for food in udcc_p[2]:
             response += food
             response += "\n"
 
-        response += "\n**Dagwood's**:\n"
-        for food in udcc_d_d:
+        response += DAGWOOD
+        for food in udcc_d[2]:
             response += food
             response += "\n"
 
-        response += "\n**Parma's**:\n"
-        for food in udcc_pa_d:
+        response += PARMA
+        for food in udcc_pa[2]:
             response += food
             response += "\n"
         await message.channel.send(response)
     if '!lunch_windows' in message.content.lower():
         response = "**Lunch** at **Friley Windows:** \n\n**Simmer**: \n"
-        for food in windows_s_l:
+        for food in windows_s[0]:
             response += food
             response += "\n"
 
-        response += "\n**Zest**:\n"
-        for food in windows_z_l:
+        response += ZEST
+        for food in windows_z[0]:
             response += food
             response += "\n"
 
-        response += "\n**Slice**:\n"
-        for food in windows_sl_l:
+        response += SLICE
+        for food in windows_sl[0]:
             response += food
             response += "\n"
 
-        response += "\n**Savor**:\n"
-        for food in windows_sv_l:
+        response += SAVOR
+        for food in windows_sv[0]:
             response += food
             response += "\n"
         await message.channel.send(response)
     if '!dinner_windows' in message.content.lower():
         response = "**Dinner** at **Friley Windows:** \n\n**Simmer**: \n"
-        for food in windows_s_d:
+        for food in windows_s[1]:
             response += food
             response += "\n"
 
-        response += "\n**Zest**:\n"
-        for food in windows_z_d:
+        response += ZEST
+        for food in windows_z[1]:
             response += food
             response += "\n"
 
-        response += "\n**Slice**:\n"
-        for food in windows_sl_d:
+        response += SLICE
+        for food in windows_sl[1]:
             response += food
             response += "\n"
 
-        response += "\n**Savor**:\n"
-        for food in windows_sv_d:
+        response += SAVOR
+        for food in windows_sv[1]:
             response += food
             response += "\n"
         await message.channel.send(response)
 
+load_menus.start()
 client.run(TOKEN)
