dbe1e396e4a1aa66e65f5ff79c35a547bb36d8fd
Ben Hall <swidexx@gmail.com>
2021-02-14 03:02:32 -0600

    fixed reload command

--- bot.py
+++ bot.py
@@ -86,7 +86,43 @@ async def on_message(message):
     CLOSED_SEASONS = "\nLooks like Seasons Marketplace is closed, or you need to reload the menus!"
 
     if '!reload' in message.content.lower():
-        load_menus()
+        print('Reloading Menus...')
+        gcontext = ssl.SSLContext()
+        request=urllib.request.Request(url_windows,None,headers)
+        with urllib.request.urlopen(request, context=gcontext) as url:
+            data = json.loads(url.read().decode())
+
+        udcc = [[[]for c in range(3)] for r in range(6)]
+        windows = [[[]]for r in range(2) for r in range(4)]
+        seasons = [[[]for c in range(3)] for r in range(6)]
+        
+        windows_bar = {'Simmer': 0, 'Zest': 1, 'Slice\'s': 2, 'Savor': 3}
+        windows_time = {'Lunch': 0, 'Dinner': 1}
+        for time in data[0]["menus"]:
+            for bar in time["menuDisplays"]:
+                for food in bar['categories'][0]['menuItems']:
+                    windows[windows_bar.get(bar['name'])][windows_time.get(time['section'])].append(food['name'])
+
+        request=urllib.request.Request(url_udcc,None,headers)
+        with urllib.request.urlopen(request) as url:
+            data = json.loads(url.read().decode())
+        udcc_bar = {'Cardinal Canteen': 0, 'Picoso': 1, 'Dagwood\'s': 2, 'Parma\'s': 3, 'Sugar Sugar': 4, 'Sprout': 5}
+        udcc_time = {'Breakfast': 0, 'Lunch': 1, 'Dinner': 2}
+        for time in data[0]["menus"]:
+            for bar in time["menuDisplays"]:
+                for food in bar['categories'][0]['menuItems']:
+                    udcc[udcc_bar.get(bar['name'])][udcc_time.get(time['section'])].append(food['name'])
+
+        request=urllib.request.Request(url_seasons,None,headers)
+        with urllib.request.urlopen(request) as url:
+            data = json.loads(url.read().decode())
+        seasons_time = {'Lunch': 0, 'Dinner': 1, 'Daily Menu': 2}
+        seasons_bar = {'Hickory\'s': 0, 'Wood Grill': 1, 'Bonsai': 2, 'Cocoa Bean': 3, 'Olive Branch': 4, 'Bushel Basket': 5}
+        for time in data[0]["menus"]:
+            for bar in time["menuDisplays"]:
+                for food in bar['categories'][0]['menuItems']:
+                    seasons[seasons_bar.get(bar['name'])][seasons_time.get(time['section'])].append(food['name'])
+        print("Complete!")
         await message.channel.send("Reloaded Menus!")
     if '!help' in message.content.lower():
         response = ""
